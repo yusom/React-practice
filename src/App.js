d91b7414be12a909cf5486eb97e7941cabd62bd4
yusom <96_chi@naver.com>
2020-11-18 11:51:46 +0900

    mode(if, else if)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Content from './components/Content';
+import Subject from './components/Subject';
+import TOC from './components/TOC';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            mode:'read',
+            subject:{title:'WEB', sub:'World Wide Web!'},
+            welcome:{title:'Welcome', desc:'Hello, React!!!'},
+            contents:[
+                {id:1, title:'HTML', desc:'HTML is for information'},
+                {id:2, title:'CSS', desc:'CSS is for design'},
+                {id:3, title:'JavaScript', desc: 'JavaScript is for interactive'}
+            ]
+        }
+    }
+    render() {
+        var _title, _desc = null;
+        if(this.state.mode === 'welcome'){
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        } else if(this.state.mode === 'read'){
+            _title = this.state.contents[0].title;
+            _desc = this.state.contents[0].desc;
+        }
+        return (
+            <div className='App'>
+                <Subject 
+                    title={this.state.subject.title} 
+                    sub={this.state.subject.sub}>
+                </Subject>
+                <TOC data={this.state.contents}>
+                </TOC>
+                <Content title={_title} desc={_desc}></Content>
+            </div>
+        );
+    }
 }
 
 export default App;
+
+//constructor(props){
+//  super(props);
+//  }
+//render 함수보다 먼저 실행하면서 그 component를 초기화시켜주고 싶은 함수
+
