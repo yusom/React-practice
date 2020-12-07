a68108e4df65bedde03a196d0de77fa58ebc40fa
yusom <96_chi@naver.com>
2020-12-07 17:40:05 +0900

    super(props)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
+import TOC from './components/TOC';
 import Content from './components/Content';
 import Subject from './components/Subject';
-import TOC from './components/TOC';
 import './App.css';
 
 class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            mode:'read',
-            subject:{title:'WEB', sub:'World Wide Web!'},
-            welcome:{title:'Welcome', desc:'Hello, React!!!'},
-            contents:[
-                {id:1, title:'HTML', desc:'HTML is for information'},
-                {id:2, title:'CSS', desc:'CSS is for design'},
-                {id:3, title:'JavaScript', desc: 'JavaScript is for interactive'}
+            mode:"read", 
+            subject:{title: "WEB", sub: "World Wide Web!"},
+            welcome:{title: "Welcome", desc: "Hello, React!"},
+            contents: [
+                {id:1, title: "HTML", desc: "HTML is for information"},
+                {id:2, title: "CSS", desc: "Css is for design"},
+                {id:3, title: "JavaScript", desc: "JavaScript is for interactive"}
             ]
         }
     }
-    render() {
+    render() { 
         var _title, _desc = null;
         if(this.state.mode === 'welcome'){
             _title = this.state.welcome.title;
@@ -29,12 +29,22 @@ class App extends Component {
         }
         return (
             <div className='App'>
-                <Subject 
+                {/* <Subject 
                     title={this.state.subject.title} 
                     sub={this.state.subject.sub}>
-                </Subject>
-                <TOC data={this.state.contents}>
-                </TOC>
+                </Subject> */}
+                <header>
+                    <h1><a href="/" onClick={function(e){
+                        console.log(e);
+                        e.preventDefault();
+                        this.state.mode = "welcome";
+                        this.setState({
+                            mode:"welcome"
+                        });
+                    }.bind(this)}>{this.state.subject.title}</a></h1>
+                    {this.state.subject.sub}
+                </header>
+                <TOC data={this.state.contents}></TOC>
                 <Content title={_title} desc={_desc}></Content>
             </div>
         );
@@ -43,8 +53,12 @@ class App extends Component {
 
 export default App;
 
-//constructor(props){
-//  super(props);
-//  }
-//render 함수보다 먼저 실행하면서 그 component를 초기화시켜주고 싶은 함수
 
+/*constructor(props){  
+    super(props);
+  }
+render 함수보다 먼저 실행하면서 그 component를 초기화시켜주고 싶은 함수 */
+
+
+/* React에선 state값이나 props의 값이 바뀌면 해당되는 component의 render함수가 호출된다
+ = 화면이 다시 그려진다 */
